bd6ab04298c2faee3d2dd70114a5519a9491be3a
iskandarprogrammer2007 <abdumalikoviskandar600@gmail.com>
2024-05-14 07:47:10 +0500

    added

--- src/pages/shopping/Shopping.jsx
+++ src/pages/shopping/Shopping.jsx
@@ -1,12 +1,34 @@
-import React, { memo } from "react";
-import Products from "../../components/productCards";
+import React, { useEffect, useState, memo } from "react";
 import "./shopping.scss";
+import mainUrl from "../../components/api/index";
+import { Link } from "react-router-dom";
 
 const Shopping = () => {
+  const [products, setProducts] = useState(null);
+  useEffect(() => {
+    mainUrl
+      .get(`/products`)
+      .then((res) => setProducts(res.data.products))
+      .catch((err) => console.log(err));
+  }, []);
+
+  let productItem = products?.map((el) => (
+    <div key={el.id} className="product__card">
+      <div className="product__card__image">
+        <img className="product__card__img" src={el.images[0]} alt={el.title} />
+      </div>
+      <div className="product__card__info">
+        <h3>{el.title}</h3>
+        <p>{el.brand}</p>
+        <p className="product__card__desc">{el.description}</p>
+      </div>
+    </div>
+  ));
+
   return (
     <div className="container shopping">
       <h1>Shop All Products</h1>
-      <Products />
+      <div className="wrapper__cards">{productItem}</div>
     </div>
   );
 };
